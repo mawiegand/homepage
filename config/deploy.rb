b53c63fe07fd674fcd00c536a5833ca2e7a334e2
Marcel Wiegand <marcel@bluepanel.org>
2016-09-06 03:16:30 +0200

    Update capistrano configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,48 +1,36 @@
 # config valid only for current version of Capistrano
-lock '3.3.5'
+lock '3.6.1'
 
 set :application, 'homepage'
 set :repo_url, 'https://github.com/mawiegand/homepage.git'
 
 # Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/var/www/virtual/wiegand/rails/homepage' 
+set :deploy_to, '/var/www/virtual/wiegand/rails/homepage'
 
 # Default value for :scm is :git
 set :scm, :git
 
-# Default value for :format is :pretty
-# set :format, :pretty
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
 
-# Default value for :log_level is :debug
-set :log_level, :info
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
 # Default value for :pty is false
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+append :linked_files, 'config/database.yml'
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
