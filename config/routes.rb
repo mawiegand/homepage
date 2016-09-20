28a8e732f7b7a30f6e9bf284636d402c0199edeb
Marcel Wiegand <marcel@bluepanel.org>
2016-09-20 05:01:59 +0200

    Add Devise views and use settings for registration
    Generate custom devise views.
    Disable registration for production environment. #12

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  SETTINGS['registration_is_enabled'] ? (devise_for :users) : (devise_for :users, skip: [:registrations])
   root 'home#show'
   resources :posts
 
