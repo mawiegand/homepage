d8b4c6f5eca6e826819ead0b2a0ce38865266b24
Marcel Wiegand <marcel@bluepanel.org>
2016-09-23 01:15:47 +0200

    Add Slider with latest blogposts to home view
    * set default order of posts
    * add method for latest posts to post model
    * add latest posts to show method of home controller
    * add slider with latest posts to home view
    
    Fixes #10

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def show
+    @posts = Post.latest
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   resourcify
   validates_presence_of :title, :body
+  default_scope { order('created_at DESC') }
+
+  def self.latest(count = SETTINGS['count_of_posts_on_index_page'])
+    limit(count)
+  end
 end
