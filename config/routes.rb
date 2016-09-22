96fc0734d35f8f93c4f8fbf0e2ac876dd97ad540
Marcel Wiegand <marcel@bluepanel.org>
2016-09-22 02:03:04 +0200

    Disable registration but allow editing profile
    Fixes #12

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  SETTINGS['registration_is_enabled'] ? (devise_for :users) : (devise_for :users, skip: [:registrations])
+  if SETTINGS['registration_is_enabled']
+    devise_for :users
+  else
+    devise_for :users, skip: :registrations
+
+    as :user do
+      delete 'users/cancel' => 'devise/registrations#cancel', :as => 'cancel_user_registration'
+      get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+      patch 'users' => 'devise/registrations#update', :as => 'user_registration'
+      delete 'users' => 'devise/registrations#destroy', :as => ''
+    end
+  end
+
   root 'home#show'
   resources :posts
 
