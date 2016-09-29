0ff8387adcb7c3083900f0189e578134eedb1b06
Marcel Wiegand <marcel@bluepanel.org>
2016-09-29 23:03:42 +0200

    Add unit tests for post model
    Related to #30

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "latest posts count from settings" do
+    assert_no_difference(SETTINGS['count_of_posts_on_index_page'].to_s) do
+      Post::latest
+    end
+  end
+
+  test "latest post count" do
+    count = 5
+    assert_no_difference(count.to_s) do
+      Post::latest(count)
+    end
+  end
 end
